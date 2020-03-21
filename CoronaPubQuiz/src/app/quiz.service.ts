743edcba67acc9b66e91d7028c6e31405e6a2253
Stephan Tietz <stephan.tietz@skipnet.de>
2020-03-21 16:20:42 +0100

    added dummy question

--- CoronaPubQuiz/src/app/quiz.service.ts
+++ CoronaPubQuiz/src/app/quiz.service.ts
@@ -8,6 +8,15 @@ export class Quiz{
   name: String;
 }
 
+export class Question{
+  question: String;
+  A: String;
+  B: String;
+  C: String;
+  D: String;
+}
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -16,14 +25,27 @@ export class QuizService {
   //quizzes: Observable<any[]>;
   quizzes: Quiz[];
 
+  questions: Question[];
+
   constructor(firestore: AngularFirestore) { 
     this.quizzes = [new Quiz(), new Quiz()]
-    //this.quizzes = firestore.collection('pubs').valueChanges();;
+    //this.quizzes = firestore.collection('pubs').valueChanges();
   }
 
 
   getQuizzes(): Observable<Quiz[]> {
     return of(this.quizzes);
+  }
+
+
+  getQuestion(): Observable<Question> {
+  	var question = new Question()
+  	question.question = 'test'
+  	question.A = '1992'
+  	question.B = '1992'
+  	question.C = '1992'
+  	question.D = '1992'
+    return of(question);
   }
 
 
