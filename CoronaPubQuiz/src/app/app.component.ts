0ac2605e2b4b854f67a7f176cee5bf6b9c12e381
Stephan Tietz <stephan.tietz@skipnet.de>
2020-03-21 13:18:37 +0100

    Initial commit

---
+++ CoronaPubQuiz/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+import { Observable } from 'rxjs';
+import 'firebase/firestore';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'CoronaPubQuiz';
+
+
+  items: Observable<any[]>;
+
+  constructor(firestore: AngularFirestore) {
+    this.items = firestore.collection('pubs').valueChanges();
+  }
+}
---
+++ CoronaPubQuiz/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyAftC4KNvWoIOGkkWKfc9K5NFwmdPk8taI",
+    authDomain: "corona-pub-quiz.firebaseapp.com",
+    databaseURL: "https://corona-pub-quiz.firebaseio.com",
+    projectId: "corona-pub-quiz",
+    storageBucket: "corona-pub-quiz.appspot.com",
+    messagingSenderId: "452835121340",
+    appId: "1:452835121340:web:5f14a59d07429920e718b9"
+  }  
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
